98601c2e0a5213690f56c92ab398e4a4d30de18a
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2025-02-07 09:31:00 +0300

    first

---
+++ php-fpm/data/www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+   <head>
+      <meta charset="UTF-8">
+      <title>HTML Document</title>
+   </head>
+   <body>
+      <?  
+      phpinfo();  
+      ?>
+   </body>
+</html>
